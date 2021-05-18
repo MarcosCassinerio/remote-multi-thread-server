633630cc7b416ce6b5ef639c05d1257fe936fb5e
LautaroCerruti <lautarocerruti7@gmail.com>
2021-05-18 18:25:33 -0300

    final changes

--- RemoteClient.c
+++ RemoteClient.c
@@ -1,13 +1,6 @@
-/* RemoteClient.c
-   Se introducen las primitivas necesarias para establecer una conexión simple
-   dentro del lenguaje C utilizando sockets.
-*/
-/* Cabeceras de Sockets */
 #include <sys/types.h>
 #include <sys/socket.h>
-/* Cabecera de direcciones por red */
 #include <netdb.h>
-/**********/
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -17,11 +10,6 @@
 #include <assert.h>
 
 #define MAX_LINE 1024
-/*
-  El archivo describe un sencillo cliente que se conecta al servidor establecido
-  en el archivo RemoteServer.c. Se utiliza de la siguiente manera:
-  $cliente IP port
- */
 
 struct addrinfo *resultado;
 pthread_t thread;
@@ -64,24 +52,20 @@ void handler(int sig) {
 int main(int argc, char **argv) {
   char buf[MAX_LINE];
 
-  /*Chequeamos mínimamente que los argumentos fueron pasados*/
   if(argc != 3){
     fprintf(stderr,"El uso es \'%s IP port\'", argv[0]);
     exit(1);
   }
 
-  /* Inicializamos el socket */
   if( (sock = socket(AF_INET , SOCK_STREAM, 0)) < 0 )
     error("No se pudo iniciar el socket");
 
-  /* Buscamos la dirección del hostname:port */
   if (getaddrinfo(argv[1], argv[2], NULL, &resultado)){
     fprintf(stderr,"No se encontro el host: %s \n",argv[1]);
     exit(2);
   }
 
   if(connect(sock, (struct sockaddr *) resultado->ai_addr, resultado->ai_addrlen) != 0)
-    /* if(connect(sock, (struct sockaddr *) &servidor, sizeof(servidor)) != 0) */
     error("No se pudo conectar :(. ");
 
   printf("La conexión fue un éxito!\n");
@@ -92,6 +76,9 @@ int main(int argc, char **argv) {
 
   for(;;) {
     recv(sock, buf, sizeof(buf),0);
+
+    if(strcmp(buf, "/server closed") == 0)
+      kill(getpid(), SIGINT);
 
     printf("%s\n", buf);
   }
--- RemoteMultiThreadServer.c
+++ RemoteMultiThreadServer.c
@@ -1,22 +1,13 @@
-/* RemoteMultiThreadServer.c */
-/* Cabeceras de Sockets */
 #include <sys/types.h>
 #include <sys/socket.h>
-/* Cabecera de direcciones por red */
 #include <netinet/in.h>
-/**********/
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-/**********/
-/* Threads! */
 #include <pthread.h>
+#include <signal.h>
 
-/* Asumimos que el primer argumento es el puerto por el cual escuchará nuestro
-servidor */
-
-/* Maxima cantidad de cliente que soportará nuestro servidor */
 #define MAX_CLIENTS 25
 #define MAX_LINE 1024
 
@@ -25,17 +16,19 @@ typedef struct _User {
   char nickname[MAX_LINE];
 }User;
 
+int sockServer;
 User users[MAX_CLIENTS];
 
 pthread_mutex_t mutex;
 
-/* Anunciamos el prototipo del hijo */
 void *child(void *arg);
-/* Definimos una pequeña función auxiliar de error */
+
 void error(char *msg);
 
+void handler(int sig);
+
 int main(int argc, char **argv){
-  int sock, *soclient, i = 0;
+  int *soclient, i = 0;
   struct sockaddr_in servidor, clientedir;
   socklen_t clientelen;
   pthread_t thread;
@@ -43,67 +36,51 @@ int main(int argc, char **argv){
 
   if (argc <= 1) error("Faltan argumentos");
 
-  /* Creamos el socket */
-  if( (sock = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+  if( (sockServer = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
     error("Socket Init");
 
-  /* Creamos a la dirección del servidor.*/
-  servidor.sin_family = AF_INET; /* Internet */
-  servidor.sin_addr.s_addr = INADDR_ANY; /**/
+  servidor.sin_family = AF_INET; 
+  servidor.sin_addr.s_addr = INADDR_ANY; 
   servidor.sin_port = htons(atoi(argv[1]));
 
-  /* Inicializamos el socket */
-  if (bind(sock, (struct sockaddr *) &servidor, sizeof(servidor)))
+  if (bind(sockServer, (struct sockaddr *) &servidor, sizeof(servidor)))
     error("Error en el bind");
 
   printf("Binding successful, and listening on %s\n",argv[1]);
 
-  /************************************************************/
-  /* Creamos los atributos para los hilos.*/
   pthread_attr_init(&attr);
-  /* Hilos que no van a ser *joinables* */
   pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
-  /************************************************************/
 
   pthread_mutex_init(&mutex, NULL);
+
+  signal(SIGINT, handler);
 
   for(; i < MAX_CLIENTS; ++i)
     users[i].socket = -1;
 
-  /* Ya podemos aceptar conexiones */
-  if(listen(sock, MAX_CLIENTS) == -1)
+  if(listen(sockServer, MAX_CLIENTS) == -1)
     error(" Listen error ");
 
-  for(;;){ /* Comenzamos con el bucle infinito*/
-    /* Pedimos memoria para el socket */
+  for(;;){
     soclient = malloc(sizeof(int));
 
-    /* Now we can accept connections as they come*/
     clientelen = sizeof(clientedir);
-    if ((*soclient = accept(sock
+    if ((*soclient = accept(sockServer
                           , (struct sockaddr *) &clientedir
                           , &clientelen)) == -1)
       error("No se puedo aceptar la conexión. ");
 
-    /* Le enviamos el socket al hijo*/
     pthread_create(&thread , NULL , child, (void *) soclient);
-
-    /* El servidor puede hacer alguna tarea más o simplemente volver a esperar*/
   }
 
-  /* Código muerto */
-  close(sock);
-
+  close(sockServer);
   return 0;
 }
 
 void * child(void *_arg){
   int socket = *(int*) _arg, i = 0, flag = 0, myId;
-  char buf[MAX_LINE] = "", msg[MAX_LINE*2] = "", nickname[MAX_LINE] = "";
+  char buf[MAX_LINE] = "", msg[MAX_LINE*2] = "", nickname[MAX_LINE] = "", aux[MAX_LINE*2] = "";
 
-  /* SEND PING! */
-  // send(socket, "PING!", sizeof("PING!"), 0);
-  /* WAIT FOR PONG! */
   while(!flag) {
     send(socket, "INGRESE UN NICKNAME:", sizeof("INGRESE UN NICKNAME:"), 0);
     recv(socket, buf, sizeof(buf), 0);
@@ -142,11 +119,13 @@ void * child(void *_arg){
   strcpy(msg, users[myId].nickname);
   strcat(msg, " ENTRO A LA SALA");
   msg[strlen(msg)] ='\0';
-
+  send(socket, "USUARIOS CONECTADOS:", sizeof("USUARIOS CONECTADOS:"), 0);
   pthread_mutex_lock(&mutex);
   for (i = 0; i < MAX_CLIENTS; ++i) {
-    if (i != myId && users[i].socket != -1) 
+    if (i != myId && users[i].socket != -1) {
       send(users[i].socket, msg, sizeof(msg), 0);
+      send(socket, users[i].nickname, sizeof(users[i].nickname), 0);
+    }
   }
   pthread_mutex_unlock(&mutex);
   
@@ -154,6 +133,7 @@ void * child(void *_arg){
   for (;;) {
     strcpy(nickname, "");
     strcpy(msg, "");
+    strcpy(aux, "");
     recv(socket, buf, sizeof(buf), 0);
     printf("soy %d [%s] en socket %d--> Recv: %s\n", myId, users[myId].nickname, socket, buf);
 
@@ -198,17 +178,34 @@ void * child(void *_arg){
             send(socket, "NICKNAME INVALIDO", sizeof("NICKNAME INVALIDO"), 0);
         } else
           send(socket, "NO SE PUEDE CAMBIAR AL NICKNAME ACTUAL", sizeof("NO SE PUEDE CAMBIAR AL NICKNAME ACTUAL"), 0);
-      } else if (strlen(buf) > 7 && buf[4] == ' ' && sscanf(buf, "/msg %s %s", nickname, msg) == 2) {
-        if (strcmp(msg, "") != 0) {
-
-        } else 
-          send(socket, "EL MENSAJE NO PUEDE ESTAR VACIO", sizeof("EL MENSAJE NO PUEDE ESTAR VACIO"), 0);
+      } else if (strlen(buf) > 7 && buf[4] == ' ' && sscanf(buf, "/msg %s %[^\n]", nickname, msg) == 2) {
+        strcpy(aux, users[myId].nickname);
+        strcat(aux, " TE SUSURRO ");
+        strcat(aux, msg);
+        pthread_mutex_lock(&mutex);
+        for (i = 0; i < MAX_CLIENTS; ++i) {
+          if (users[i].socket != -1 && strcmp(nickname, users[i].nickname) == 0) {
+            send(users[i].socket, aux, sizeof(aux), 0);
+            break;
+          }
+        }
+        if (i == MAX_CLIENTS)
+          send(socket, "NICKNAME NO ENCONTRADO", sizeof("NICKNAME NO ENCONTRADO"), 0);
+        pthread_mutex_unlock(&mutex);
       } else
         send(socket, "COMANDO INVALIDO", sizeof("COMANDO INVALIDO"), 0);
     }
   }
+
+  strcpy(msg, users[myId].nickname);
+  strcat(msg, " SE FUE DE LA SALA");
+  msg[strlen(msg)] ='\0';
   pthread_mutex_lock(&mutex);
-  users[i].socket = -1;
+  for (i = 0; i < MAX_CLIENTS; ++i) {
+    if (i != myId && users[i].socket != -1) 
+      send(users[i].socket, msg, sizeof(msg), 0);
+  }
+  users[myId].socket = -1;
   pthread_mutex_unlock(&mutex);
   free((int*)_arg);
   return NULL;
@@ -216,4 +213,18 @@ void * child(void *_arg){
 
 void error(char *msg){
   exit((perror(msg), 1));
+}
+
+void handler(int sig) {
+  int i = 0;
+  pthread_mutex_lock(&mutex);
+  for (; i < MAX_CLIENTS; ++i)
+  {
+    if (users[i].socket != -1)
+      send(users[i].socket, "/server closed", sizeof("/server closed"), 0);
+  }
+  pthread_mutex_unlock(&mutex);
+  
+  close(sockServer);
+  exit(EXIT_SUCCESS);
 }
